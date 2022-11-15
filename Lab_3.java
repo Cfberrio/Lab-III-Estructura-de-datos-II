e09f699da7d5df1f9f9646fd1a26e9c079dd5b1c
EstebanPerezMendoza <98895040+EstebanPerezMendoza@users.noreply.github.com>
2022-11-15 01:25:55 -0500

    Add files via upload

---
+++ Lab_3.java
@@ -0,0 +1,220 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_3;
+
+/**
+ *
+ * @author user
+ */
+import java.util.Scanner;
+
+public class Lab_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int arrh[] = {12, 11, 13, 5, 6, 7};
+        int arrm[] = {62, 31, 15, 4, 9, 7};
+        int arrq[] = {64, 51, 15, 6, 9, 43};
+        int N = arrh.length;
+        int Nm = arrm.length;
+        int Nq = arrq.length;
+        HeapSort hs = new HeapSort();
+        Merge ms = new Merge();
+        Quick qs = new Quick();
+        hs.sort(arrh);
+        ms.mergeSort(arrm, 0, Nm - 1);
+        qs.quickSort(arrq, 0, Nq - 1);
+        System.out.println("Sorted array is");
+        printArray(arrh);
+        printArray(arrm);
+        printArray(arrq);
+    }
+
+    public static class HeapSort {
+
+        public void sort(int arr[]) {
+            int N = arr.length;
+
+            // Build heap (rearrange array)
+            for (int i = N / 2 - 1; i >= 0; i--) {
+                heapify(arr, N, i);
+            }
+
+            // One by one extract an element from heap
+            for (int i = N - 1; i > 0; i--) {
+                // Move current root to end
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                // call max heapify on the reduced heap
+                heapify(arr, i, 0);
+            }
+        }
+
+        // To heapify a subtree rooted with node i which is
+        // an index in arr[]. n is size of heap
+        void heapify(int arr[], int N, int i) {
+            int largest = i; // Initialize largest as root
+            int l = 2 * i + 1; // left = 2*i + 1
+            int r = 2 * i + 2; // right = 2*i + 2
+
+            // If left child is larger than root
+            if (l < N && arr[l] > arr[largest]) {
+                largest = l;
+            }
+
+            // If right child is larger than largest so far
+            if (r < N && arr[r] > arr[largest]) {
+                largest = r;
+            }
+
+            // If largest is not root
+            if (largest != i) {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                // Recursively heapify the affected sub-tree
+                heapify(arr, N, largest);
+            }
+        }
+
+        /* A utility function to print array of size n */
+        // Driver's code
+    }
+
+    static void printArray(int arr[]) {
+        int N = arr.length;
+
+        for (int i = 0; i < N; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static class Merge {
+
+        /* Function to merge the subarrays of a[] */
+        void merge(int a[], int beg, int mid, int end) {
+            int i, j, k;
+            int n1 = mid - beg + 1;
+            int n2 = end - mid;
+
+            /* temporary Arrays */
+            int LeftArray[] = new int[n1];
+            int RightArray[] = new int[n2];
+
+            /* copy data to temp arrays */
+            for (i = 0; i < n1; i++) {
+                LeftArray[i] = a[beg + i];
+            }
+            for (j = 0; j < n2; j++) {
+                RightArray[j] = a[mid + 1 + j];
+            }
+
+            i = 0;
+            /* initial index of first sub-array */
+            j = 0;
+            /* initial index of second sub-array */
+            k = beg;
+            /* initial index of merged sub-array */
+
+            while (i < n1 && j < n2) {
+                if (LeftArray[i] <= RightArray[j]) {
+                    a[k] = LeftArray[i];
+                    i++;
+                } else {
+                    a[k] = RightArray[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1) {
+                a[k] = LeftArray[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2) {
+                a[k] = RightArray[j];
+                j++;
+                k++;
+            }
+        }
+
+        void mergeSort(int a[], int beg, int end) {
+            if (beg < end) {
+                int mid = (beg + end) / 2;
+                mergeSort(a, beg, mid);
+                mergeSort(a, mid + 1, end);
+                merge(a, beg, mid, end);
+            }
+        }
+    }
+
+    public static class Quick {
+
+        // A utility function to swap two elements
+        static void swap(int[] arr, int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        /* This function takes last element as pivot, places
+       the pivot element at its correct position in sorted
+       array, and places all smaller (smaller than pivot)
+       to left of pivot and all greater elements to right
+       of pivot */
+        static int partition(int[] arr, int low, int high) {
+
+            // pivot
+            int pivot = arr[high];
+
+            // Index of smaller element and
+            // indicates the right position
+            // of pivot found so far
+            int i = (low - 1);
+
+            for (int j = low; j <= high - 1; j++) {
+
+                // If current element is smaller
+                // than the pivot
+                if (arr[j] < pivot) {
+
+                    // Increment index of
+                    // smaller element
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+            swap(arr, i + 1, high);
+            return (i + 1);
+        }
+
+        /* The main function that implements QuickSort
+              arr[] --> Array to be sorted,
+              low --> Starting index,
+              high --> Ending index
+         */
+        static void quickSort(int[] arr, int low, int high) {
+            if (low < high) {
+
+                // pi is partitioning index, arr[p]
+                // is now at right place
+                int pi = partition(arr, low, high);
+
+                // Separately sort elements before
+                // partition and after partition
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+            }
+        }
+    }
+}
